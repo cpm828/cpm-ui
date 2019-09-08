403bcd56bf8f3a44776df77ea1d77aca496ab57f
pengmin.chen <pengmin.chen@17zuoye.com>
2019-09-08 19:44:12 +0800

    fix

--- examples/src/views/Toast.vue
+++ examples/src/views/Toast.vue
@@ -16,8 +16,12 @@
         <code-block :code="item.code" v-if="item.showCode"></code-block>
       </div>
     </div>
-    
-    <a class="aLink" c_wrap="fs-12" href="https://github.com/cpm828/cpm828.github.io/blob/master/cpm_ui/document/Toast.md">查看文档</a>
+
+    <a
+      class="aLink"
+      c_wrap="fs-12"
+      href="https://github.com/cpm828/cpm828.github.io/blob/master/cpm_ui/document/Toast.md"
+    >查看文档</a>
   </div>
 </template>
 
@@ -45,12 +49,24 @@ export default {
 })`
         },
         {
-          title: '自定义位置',
+          title: '选择显示位置',
           showCode: false,
           code: `this.$toast({
   propsData: {
-    message: '3s后关闭',
+    message: '选择显示位置',
     position: 'top'
+  }
+})`
+        },
+        {
+          title: '自定义toast样式',
+          showCode: false,
+          code: `this.$toast({
+  propsData: {
+    message: '自定义toast样式',
+    wrapStyleObj: {
+      'background': '#666'
+    }
   }
 })`
         },
@@ -60,8 +76,7 @@ export default {
           code: `this.$toast({
   propsData: {
     message: '带icon的toast',
-    type: 'success',
-    time: 3000
+    type: 'success'
   }
 })
  `
@@ -72,22 +87,20 @@ export default {
           code: `this.$toast({
   propsData: {
     message: '带倒计时回调的toast',
-    type: 'success',
-    time: 2000
+    type: 'success'
+  },
+  methods: {
+    callback () {
+      alert('toast 消失了')
+    }
   }
-}),
-methods: {
-  callback () {
-    alert('toast 消失了')
-  }
-}
+})
  `
         },
         {
           title: '全部参数',
           showCode: false,
-          code: `
-this.$toast({
+          code: `this.$toast({
   propsData: {
     message: '全部传参',
     type: 'info',
@@ -122,9 +135,11 @@ this.$toast({
     showToast (index) {
       this[`showToast${index + 1}`]()
     },
+    // 普通toast
     showToast1 () {
       this.$toast('普通toast')
     },
+    // 自定义关闭时间
     showToast2 () {
       this.$toast({
         propsData: {
@@ -133,34 +148,42 @@ this.$toast({
         }
       })
     },
+    // 选择显示位置
     showToast3 () {
       this.$toast({
         propsData: {
-          message: '3s后关闭',
+          message: '选择显示位置',
           position: 'top'
         }
       })
     },
+    // 自定义toast样式
     showToast4 () {
-      // 注：调用方法除props改用propsData外，其余都与组件内部一致，可使用propsData、data、computed、methods、watch
       this.$toast({
         propsData: {
-          message: '带消失回调的toast',
-          time: 3000
-        },
-        methods: {
-          callback () {
-            alert('toast 消失了')
+          message: '自定义toast样式',
+          wrapStyleObj: {
+            'background': '#666'
           }
         }
       })
     },
+    // 带icon的toast
     showToast5 () {
-      // 注：调用方法除props改用propsData外，其余都与组件内部一致，可使用propsData、data、computed、methods、watch
       this.$toast({
         propsData: {
-          message: 'toast文本内容4',
+          message: '带icon的toast',
+          type: 'success',
           time: 3000
+        }
+      })
+    },
+    // 带倒计时回调的toast
+    showToast6 () {
+      this.$toast({
+        propsData: {
+          message: '带倒计时回调的toast',
+          type: 'success'
         },
         methods: {
           callback () {
@@ -169,6 +192,26 @@ this.$toast({
         }
       })
     },
+    // 全部参数
+    showToast7 () {
+      // 注：调用方法除props改用propsData外，其余都与组件内部一致，可使用propsData、data、computed、methods、watch
+      this.$toast({
+        propsData: {
+          message: '全部传参',
+          type: 'info',
+          time: 3000,
+          position: 'middle',
+          wrapStyleObj: {
+            'background': '#4c4c4c'
+          }
+        },
+        methods: {
+          callback () {
+            alert('toast 消失了')
+          }
+        }
+      })
+    }
   }
 }
 </script>
